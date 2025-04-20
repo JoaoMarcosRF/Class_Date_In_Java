d73f8b4ac4945fceb8d16b92fa7efccfdb334eb5
JoaoMarcosRF <jmarcs.rf@gmail.com>
2025-04-20 18:23:11 -0300

    new constructor for Date class

--- classDate.java
+++ classDate.java
@@ -3,7 +3,17 @@ package Classe_Objetos.Date;
 public class classDate{
     int day,month, year;
 
-    public void showDate(){
-        System.out.printf("%d/%d/%d", day, month, year);
+    public classDate(){
+
+    }
+
+    public classDate(int iDay, int iMonth, int iYear){
+        iDay = day;
+        iMonth = day;
+        iYear = day;
+    }
+
+    public String showDate(){
+        return String.format("%d/%d/%d", day, month, year);
     }
 }
