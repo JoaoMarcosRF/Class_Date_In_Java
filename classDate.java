91ffcad54e25b2bb26f76122b5b3b54ad2e0c9c9
JoaoMarcosRF <jmarcs.rf@gmail.com>
2025-04-20 17:56:04 -0300

    first commit

---
+++ classDate.java
@@ -0,0 +1,9 @@
+package Classe_Objetos.Date;
+
+public class classDate{
+    int day,month, year;
+
+    public void exibirData(){
+        System.out.printf("%d/%d/%d", day, month, year);
+    }
+}
---
+++ implementationOfClassData.java
@@ -0,0 +1,34 @@
+package Classe_Objetos.Date;
+import java.util.Scanner;
+
+
+public class implementationOfClassData {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Insira o numero de datas que voce quer inserir: ");
+        int rangeOfDates = input.nextInt(); 
+
+        classDate[] datas = new classDate[rangeOfDates];
+        
+        for (int i = 0 ; i < rangeOfDates ; i++) {
+
+            datas[i] = new classDate();
+
+            System.out.printf("Insira o dia da data[%d]: ",i + 1);
+            datas[i].day = input.nextInt();
+            System.out.printf("Insira o mês da data[%d]: ",i + 1);
+            datas[i].month = input.nextInt();
+            System.out.printf("Insira o ano da data[%d]: ",i + 1);
+            datas[i].year = input.nextInt();
+        }
+
+        System.out.println("Datas informadas: ");
+        for (int i = 0 ; i < rangeOfDates ; i++ ) {
+            datas[i].exibirData();
+        }
+
+        input.close();
+    }
+}
